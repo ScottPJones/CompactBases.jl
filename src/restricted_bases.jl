fb2573fc2e7860ad6fd568659cece64e15bb31d1
Stefanos Carlström <jagot@users.noreply.github.com>
2022-09-14 10:04:55 +0200

    Simplify {Adjoint,}BasisOrRestricted to avoid problems downstream (#59)

--- src/restricted_bases.jl
+++ src/restricted_bases.jl
@@ -4,8 +4,8 @@ const RestrictionMatrix = BandedMatrix{<:Int, <:FillArrays.Ones}
 const RestrictedQuasiArray{T,N,B<:Basis} = SubQuasiArray{T,N,B}
 const AdjointRestrictedQuasiArray{T,N,B<:Basis} = QuasiAdjoint{T,<:RestrictedQuasiArray{T,N,B}}
 
-const BasisOrRestricted{B<:Basis} = Union{B,<:RestrictedQuasiArray{<:Any,<:Any,<:B}}
-const AdjointBasisOrRestricted{B<:Basis} = Union{<:QuasiAdjoint{<:Any,B},<:AdjointRestrictedQuasiArray{<:Any,<:Any,<:B}}
+const BasisOrRestricted{B<:Basis} = Union{B,<:RestrictedQuasiArray{<:Any,<:Any,B}}
+const AdjointBasisOrRestricted{B<:Basis} = Union{<:QuasiAdjoint{<:Any,B},<:AdjointRestrictedQuasiArray{<:Any,<:Any,B}}
 
 indices(B::Basis) = axes(B)
 indices(B::RestrictedQuasiArray) = B.indices
